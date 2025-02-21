ca9319e4f9a14b8315c8354e3cbd453d5b4cf987
MEGA <MEGA@gmail.com>
2025-02-21 12:19:57 +0200

    Add babylon

--- ClassC.cs
+++ ClassC.cs
@@ -9,5 +9,16 @@ namespace ConsoleApp1
     internal class ClassC
     {
 
+
+        public double BabyLoonSqrt(double number)
+        {
+            double guess = number / 2;
+            while(Math.Abs(guess*guess - number ) < 0.01){
+                double other = number / guess;
+                guess = (guess + other) / 2;
+            }
+            return guess;
+        }
+
     }
 }
